7320e7f0a940f9644a43c8fb192e21b878a0034d
Sen Hongo <sen@legendofcode.com>
2023-04-06 04:48:01 +0900

    :+1: method for adding data to firebase

---
+++ src/features/weight/model.ts
@@ -0,0 +1,17 @@
+import { setDoc, doc, Timestamp } from 'firebase/firestore';
+
+import { db } from '@/libs/firebase';
+
+export type WeightRecord = {
+  weight: number;
+  bodyFatRatio: number;
+  date: string;
+};
+
+export async function addData(data: WeightRecord) {
+  await setDoc(doc(db, 'weight', data.date), {
+    weight: data.weight,
+    bodyFatRatio: data.bodyFatRatio,
+    date: Timestamp.fromDate(new Date(data.date)),
+  });
+}
