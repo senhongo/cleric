b21a4cc5975b6eea667f0d3a057f26a38eb7267f
Sen Hongo <sen@legendofcode.com>
2023-04-06 04:44:58 +0900

    :+1: add firebase

---
+++ src/libs/firebase/index.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_apiKey,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_authDomain,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_projectId,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_storageBucket,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_messagingSenderId,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_appId,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_measurementId,
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
